34466aa56ba28dd743f4d850d5dc11bb6cfde383
nikhlsunil90s <nikhlsunil90s@gmail.com>
2021-08-09 18:42:13 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import Navbar from './component/Navbar'
 import Home from'./component/Home'
 import Checkout from './component/Checkout'
-import About from './component/About'
-import Contact from './component/Contact'
-import Service from './component/Service'
-import EditUsers from './component/editUser'
+
 import View from './component/view'
 
 function App() {
--- src/component/Checkout.jsx
+++ src/component/Checkout.jsx
@@ -5,7 +5,15 @@ import { Link } from 'react-router-dom'
 
 const Checkout = ()=> {
   const { id } = useParams();
-
+  const [item, setItem] = useState({
+    name: "",
+    brand: "",
+    price: "",
+    available: "",
+    weight: "",
+    options: []
+  });
+  const { name, brand, price, options, weight } = item;
   useEffect(()=>{
      loadData();
     }
@@ -14,6 +22,7 @@ const Checkout = ()=> {
   const loadData= async ()=>{
       const result = await Axios.get(`http://localhost:3003/items/${id}`);
       console.log(result.data);
+        setItem(result.data)
   }
   return (
     <>
@@ -21,6 +30,23 @@ const Checkout = ()=> {
         <h2 className="text-center">Checkout Page!</h2>
             <p className="text-center">Confirm The Product Details Below:</p>
             
+            <div className="row">
+                <div className="col-md-6">
+                    <p>Name: {name}</p>
+                </div>
+                <div className="col-md-6">
+                    <p>Color: </p>
+                    {
+                        options && options.map(opt => {
+                            return <span style={{ backgroundColor: opt.color, color: opt.color, height: '50px', width: '50px', border: '1px solid lightblue', borderRadius: '50%', padding: '2px 8px'  }}> </span>
+                        })
+                    }
+
+
+                    <p>Price: {price}</p>
+                </div>
+
+            </div>
     </div>
     </>
     )
